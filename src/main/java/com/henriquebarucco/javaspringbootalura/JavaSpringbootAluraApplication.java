86e5861fa251cb8926200b79d5f6b7c1869e7af4
Henrique Barucco <henrique_barucco@hotmail.com>
2022-12-04 21:22:49 -0300

    Innitial commit

---
+++ src/main/java/com/henriquebarucco/javaspringbootalura/JavaSpringbootAluraApplication.java
@@ -0,0 +1,13 @@
+package com.henriquebarucco.javaspringbootalura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringbootAluraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringbootAluraApplication.class, args);
+    }
+
+}
