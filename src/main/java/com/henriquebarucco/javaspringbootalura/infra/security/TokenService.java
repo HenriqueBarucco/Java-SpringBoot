73fb0ca19bfe9939de9b9e6190b9b36c1c666b38
Henrique Barucco <henrique_barucco@hotmail.com>
2022-12-24 19:13:44 -0300

    Controle de acesso

--- src/main/java/com/henriquebarucco/javaspringbootalura/infra/security/TokenService.java
+++ src/main/java/com/henriquebarucco/javaspringbootalura/infra/security/TokenService.java
@@ -3,6 +3,7 @@ package com.henriquebarucco.javaspringbootalura.infra.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.henriquebarucco.javaspringbootalura.domain.usuario.Usuario;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -28,8 +29,19 @@ public class TokenService {
         } catch (JWTCreationException exception) {
             throw new RuntimeException("erro ao gerar token jwt", exception);
         }
-
     }
+
+    public String getSubject(String tokenJWT) {
+        try {
+            var algoritmo = Algorithm.HMAC256(secret);
+            return JWT.require(algoritmo)
+                    .withIssuer("API Voll.med")
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+        } catch (JWTVerificationException exception) {
+            throw new RuntimeException("Token JWT inválido ou expirado!");
+        }}
 
     private Instant dataExpiracao() {
         return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
