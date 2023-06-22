c3f14ff32bb38626bf92cc2b2ada236d12f34d0b
Henrique Barucco <henrique_barucco@hotmail.com>
2023-06-22 11:50:03 -0300

    feat: adiciona dependência do SpringDoc e configurações do Swagger Adiciona a dependência do SpringDoc para gerar a documentação do Swagger e configurações do Swagger para permitir o acesso à documentação da API. Também adiciona a anotação @SecurityRequirement(name = "bearer-key") nos controllers para exigir autenticação JWT em todas as rotas. Por fim, adiciona a classe SpringDocConfigurations para personalizar a documentação do Swagger.

--- src/main/java/com/henriquebarucco/javaspringbootalura/controller/ConsultaController.java
+++ src/main/java/com/henriquebarucco/javaspringbootalura/controller/ConsultaController.java
@@ -4,6 +4,7 @@ import com.henriquebarucco.javaspringbootalura.domain.consulta.AgendaDeConsultas
 import com.henriquebarucco.javaspringbootalura.domain.consulta.DadosAgendamentoConsulta;
 import com.henriquebarucco.javaspringbootalura.domain.consulta.DadosCancelamentoConsulta;
 import com.henriquebarucco.javaspringbootalura.domain.consulta.DadosDetalhamentoConsulta;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("consultas")
+@SecurityRequirement(name = "bearer-key")
 public class ConsultaController {
 
     @Autowired
--- src/main/java/com/henriquebarucco/javaspringbootalura/controller/PacienteController.java
+++ src/main/java/com/henriquebarucco/javaspringbootalura/controller/PacienteController.java
@@ -1,6 +1,7 @@
 package com.henriquebarucco.javaspringbootalura.controller;
 
 import com.henriquebarucco.javaspringbootalura.domain.paciente.*;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -13,6 +14,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("pacientes")
+@SecurityRequirement(name = "bearer-key")
 public class PacienteController {
 
     @Autowired
