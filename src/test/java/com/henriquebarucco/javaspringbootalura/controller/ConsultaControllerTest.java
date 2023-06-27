7cb4b565d7e4feae93c9b81d10b5fb4fec50c3fb
Henrique Barucco <henrique_barucco@hotmail.com>
2023-06-27 13:55:36 -0300

    feat(db): adiciona coluna 'ativo' na tabela 'pacientes' e define valor padrão como 1 feat(ConsultaControllerTest.java): adiciona teste para agendamento de consulta com informações válidas Adiciona uma nova coluna 'ativo' na tabela 'pacientes' e define o valor padrão como 1. Isso permite que o sistema possa filtrar pacientes ativos e inativos. O teste adicionado verifica se o agendamento de uma consulta com informações válidas retorna um código HTTP 200 e um JSON com os detalhes da consulta agendada.

--- src/test/java/com/henriquebarucco/javaspringbootalura/controller/ConsultaControllerTest.java
+++ src/test/java/com/henriquebarucco/javaspringbootalura/controller/ConsultaControllerTest.java
@@ -1,24 +1,46 @@
 package com.henriquebarucco.javaspringbootalura.controller;
 
+import com.henriquebarucco.javaspringbootalura.domain.consulta.AgendaDeConsultas;
+import com.henriquebarucco.javaspringbootalura.domain.consulta.DadosAgendamentoConsulta;
+import com.henriquebarucco.javaspringbootalura.domain.consulta.DadosDetalhamentoConsulta;
+import com.henriquebarucco.javaspringbootalura.domain.medico.Especialidade;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDateTime;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@AutoConfigureJsonTesters
 class ConsultaControllerTest {
 
     @Autowired
     private MockMvc mvc;
+
+    @Autowired
+    private JacksonTester<DadosAgendamentoConsulta> dadosAgendamentoConsultaJson;
+
+    @Autowired
+    private JacksonTester<DadosDetalhamentoConsulta> dadosDetalhamentoConsultaJson;
+
+    @MockBean
+    private AgendaDeConsultas agendaDeConsultas;
 
     @Test
     @DisplayName("Deveria devolver código http 400 quando informações estão inválidas")
@@ -28,5 +50,35 @@ class ConsultaControllerTest {
                 .andReturn().getResponse();
 
         assertThat(response.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
+
+    @Test
+    @DisplayName("Deveria devolver código http 200 quando informações estão válidas")
+    @WithMockUser
+    void agendar_cenario2() throws Exception {
+        var data = LocalDateTime.now().plusHours(1);
+        var especialidade = Especialidade.CARDIOLOGIA;
+
+        var dadosDetalhamento = new DadosDetalhamentoConsulta(null, 2L, 5L, data);
+
+        when(agendaDeConsultas.agendar(any())).thenReturn(dadosDetalhamento);
+
+        var response = mvc
+                .perform(
+                        post("/consultas")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(dadosAgendamentoConsultaJson.write(
+                                        new DadosAgendamentoConsulta(2L, 5L, data, especialidade)
+                                ).getJson())
+                )
+                .andReturn().getResponse();
+
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+
+        var jsonEsperado = dadosDetalhamentoConsultaJson.write(
+                dadosDetalhamento
+        ).getJson();
+
+        assertThat(response.getContentAsString()).isEqualTo(jsonEsperado);
     }
 }
